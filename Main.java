82766cf7b6b4fba2bae28e333748c19f01e451ae
Peeoner174 <Peeoner174@gmail.com>
2017-02-14 10:35:33 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
+//Конструктор с одним параметром типа String класса ConsolePrinter 
+//позволяет создать новый объект этого класса и указать для него 
+//новое значение переменной text.
+//Ключесое слово final в 10 cтроке запрещает изменения значения 
+//String text для всех объектов данного класса.
 public class Main {
 
     private static class ConsolePrinter implements Runnable {
 
-        private final String text = "my text";
+        private String text = "my text";      //Ошибка была здесь, а именно - ключевое слово final 
         private ConsolePrinter(String text) {
             this.text = text;
         }
