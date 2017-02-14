9e54bc76811dfa88057a3abaf61799fe8949ba64
Peeoner174 <Peeoner174@gmail.com>
2017-02-14 10:48:29 +0500

    Задание 3
    Составить код программы на языке Java, который выполняет функцию ввода текста с права на лева и изменением его написания слева на право (Пример: hello - olleh) Ответ - ссылка на gist с программой.

---
+++ Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+ 
+    public static void main(String[] args) {
+        String s ="hello word";
+        StringBuilder revStr = new StringBuilder(s);
+        System.out.println(revStr.reverse());
+      }
+}
+
